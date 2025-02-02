094156227ce376b281e82ae3c65fe1b9af2427ea
Michelle Sun <lu.yingshan@gmail.com>
2025-02-02 14:10:17 -0800

    Lab1 Submission

--- lab1/README.md
+++ lab1/README.md
@@ -1,17 +1,30 @@
-## UID: 123456789
+## UID: 706001504
 
 ## Pipe Up
 
-One sentence description
+This program imitates the pipe operator (|) by taking in command lines as arguments.
 
 ## Building
 
-Explain briefly how to build your program
+```
+make
+```
 
 ## Running
 
 Show an example run of your program, using at least two additional arguments, and what to expect
 
+Here is an example run of my program on the cs111 VM, where we can expect the output of both lines to be the same.
+```
+cs111@cs111 Desktop/lab1 » ls | cat | wc                                                        
+      7       7      63
+cs111@cs111 Desktop/lab1 » ./pipe ls cat wc
+      7       7      63
+```
+
 ## Cleaning up
 
-Explain briefly how to clean up all binary files
+To clean up all binary files:
+```
+make clean
+```
--- lab1/pipe.c
+++ lab1/pipe.c
@@ -3,9 +3,102 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <errno.h>
 
 int main(int argc, char *argv[])
 {
-	// TODO: it's all yours
+	// 0 args: error & exit
+	if (argc <= 1) {
+		errno = EINVAL;
+		perror("EINVAL: (0 args)");
+		exit(errno);
+	}
+
+	// discussion method: fork and pipe combo <3
+	int pipe_old[2]; // 0 = read end | 1 = write end
+	int pipe_new[2];
+
+	int last = argc - 1;
+
+	for (int i = 1; i < argc; i++) {
+
+		// 1 arg: run the program & return
+		if (argc == 2) {
+			if (execlp(argv[1], argv[1], NULL) == -1) { // failed execution
+				perror("Failed to run a singular program (1 arg)");
+				exit(errno);
+			}
+			return 0; // successful
+		} 
+
+		// create a new pipe (unless not last arg)
+		if (i != last)
+			if (pipe(pipe_new) == -1) // failed pipe
+			{
+				perror("Failed to create pipe");
+				exit(errno);
+			}
+
+		// FORK: create child process
+		int pid = fork();
+
+		// fork failed
+		if (pid < 0) {
+			perror("Failed to fork");
+			exit(errno);
+		}
+
+		// child process
+		if (pid == 0) {
+			// read from previous pipe (if not 1st arg)
+			if (i > 1) {
+				dup2(pipe_old[0], STDIN_FILENO);
+				close(pipe_old[0]);
+			}
+
+			// output into new pipe (if not last arg)
+			if (i != last) {
+				dup2(pipe_new[1], STDOUT_FILENO);
+			}
+
+			// done with new pipe
+			close(pipe_new[0]);
+			close(pipe_new[1]);
+
+			// execute the arg
+			if (execlp(argv[i], argv[i], NULL) == -1) {
+				perror("Failed to execute arg in child");
+				exit(errno);
+			}
+			
+			return 0;
+		} else { // parent process
+			// wait for child & store child's exit status into 'status'
+			int status;
+			if (wait(&status) == -1) {
+				perror("Failed to wait for child process");
+				exit(errno);
+			}
+			if (WEXITSTATUS(status)) { // catch errors in child (exit status != 0)
+				perror("Parent caught error in child process");
+				return (WEXITSTATUS(status));
+			}
+
+			// store new -> old pipe, so that you can create new pipe for next arg
+			if (i != last) {
+				// done writing to new pipe! but don't delete it
+				close(pipe_new[1]);
+				pipe_old[0] = pipe_new[0];
+				pipe_old[1] = pipe_new[1];
+			} else {
+				// last arg: we are done, close everything!
+				close(pipe_old[0]);
+				close(pipe_old[1]);
+				close(pipe_new[0]);
+				close(pipe_new[1]);
+			}
+		}
+	}
+
 	return 0;
 }
