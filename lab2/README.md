d39bdd175af70138992a7361f6b943524c1fffbb
Michelle Sun <lu.yingshan@gmail.com>
2025-02-16 22:32:46 -0800

    Finished Lab 2

--- lab2/README.md
+++ lab2/README.md
@@ -1,28 +1,37 @@
 # You Spin Me Round Robin
 
-TODO
+This project implements a Round Robin (RR) CPU scheduling algorithm in C using a doubly linked list (TAILQ) to manage processes, ensuring fair execution by enforcing a fixed time quantum. It tracks waiting time and response time, handling process arrivals, quantum expiration, and completion, then outputs the average times for evaluation.
 
 ## Building
 
 ```shell
-TODO
+make
 ```
 
 ## Running
 
-cmd for running TODO
+Example of processes.txt:
 ```shell
-TODO
+4
+1, 0, 7
+2, 2, 4
+3, 4, 1
+4, 5, 4
 ```
 
-results TODO
+Example of cmd for running ./rr
 ```shell
-TODO
+./rr processes.txt 3
+```
 
+Results :
+```shell
+Average waiting time: 7.00
+Average response time: 2.75
 ```
 
 ## Cleaning up
 
 ```shell
-TODO
+make clean
 ```
--- lab2/rr.c
+++ lab2/rr.c
@@ -14,18 +14,27 @@ typedef int32_t i32;
 
 struct process
 {
-  u32 pid;
-  u32 arrival_time;
-  u32 burst_time;
+  u32 pid;            // process ID
+  u32 arrival_time;   // when the process arrives
+  u32 burst_time;     // total execution time required
 
   TAILQ_ENTRY(process) pointers;
 
   /* Additional fields here */
+
+  u32 response_time;
+  u32 waiting_time;
+  u32 time_started;
+  u32 time_left;
+  
   /* End of "Additional fields here" */
 };
 
-TAILQ_HEAD(process_list, process);
+TAILQ_HEAD(process_list, process); // doubly linked list of process
 
+// reads next int from a character buffer
+// skips non-digit characters
+// extracts and constructs an integer value from the file data
 u32 next_int(const char **data, const char *data_end)
 {
   u32 current = 0;
@@ -62,6 +71,8 @@ u32 next_int(const char **data, const char *data_end)
   exit(EINVAL);
 }
 
+// converts a C-string number --> an integer
+// stops if it finds a non-numeric character
 u32 next_int_from_c_str(const char *data)
 {
   char c;
@@ -88,6 +99,8 @@ u32 next_int_from_c_str(const char *data)
   return current;
 }
 
+// opens the input file (processes.txt)
+// stores for each process: .pid .arrival_time .burst_time
 void init_processes(const char *path,
                     struct process **process_data,
                     u32 *process_size)
@@ -147,20 +160,84 @@ int main(int argc, char *argv[])
   {
     return EINVAL;
   }
-  struct process *data;
-  u32 size;
-  init_processes(argv[1], &data, &size);
+  struct process *data;   // pointer to the beginning of the memory-mapped file
+  u32 size;               // size of input file (in bytes)
+  init_processes(argv[1], &data, &size);  // read processes from input file
 
-  u32 quantum_length = next_int_from_c_str(argv[2]);
+  u32 quantum_length = next_int_from_c_str(argv[2]); // quantum time 
 
   struct process_list list;
-  TAILQ_INIT(&list);
+  TAILQ_INIT(&list);          // initialize process queue
 
   u32 total_waiting_time = 0;
   u32 total_response_time = 0;
 
   /* Your code here */
+
+  for (u32 i = 0; i < size; i++)// loop thru all processes --> set up all processes
+  {
+    data[i].time_started = -1;               // none have started
+    data[i].time_left = data[i].burst_time;  // reminaing time = total exec time required
+  }
+
+  struct process* stored_process;  // stores a process that exceeded its quantum
+  u32 curr_time_step = 0;          // current time step
+  u32 time_slice_elapsed = 0;      // how long curr process has been running
+  u32 finished_count = 0;          // # of completed processes
+  u32 needs_requeue = 0;           // if it needs to be reinserted
   
+  while (finished_count < size){  // loop until all processes are completed
+
+    for (u32 i = 0; i < size; i++){ // loop thru processes --> prioritize appending newly arrived processes
+      if (data[i].arrival_time == curr_time_step){
+        struct process *arrived_process = &data[i];
+        TAILQ_INSERT_TAIL(&list, arrived_process, pointers);
+      }
+    }
+
+    if (needs_requeue == 1){ // if needs to be reinserted into the queue --> reinsert (after arrived ^)
+      needs_requeue = 0;
+      TAILQ_INSERT_TAIL(&list, stored_process, pointers);
+    }
+
+    struct process* curr_process;
+    if (!TAILQ_EMPTY(&list)){ // if queue not empty
+      curr_process = TAILQ_FIRST(&list); // first on queue
+
+      time_slice_elapsed += 1;
+      curr_process->time_left -= 1;
+
+      if (curr_process->time_started == -1){ // has not started yet --> start it
+        curr_process->response_time = curr_time_step - curr_process->arrival_time; // curr_time_step - arrival
+        curr_process->time_started = curr_time_step;
+      }
+
+      if (curr_process->time_left == 0){ // finished
+        curr_process->waiting_time = curr_time_step + 1 - curr_process->arrival_time - curr_process->burst_time; // curr_time_step + 1 - arrival - burst
+        time_slice_elapsed = 0;
+        finished_count += 1;
+        TAILQ_REMOVE(&list, curr_process, pointers);
+        // printf("🍅 Finished: current time step = %d\n", curr_time_step);
+        // printf("🍅 -----: pid = %d\n", curr_process->pid);
+      }
+      
+      if (curr_process->time_left != 0 && time_slice_elapsed == quantum_length){ // pre-empt
+	      stored_process = curr_process;
+        time_slice_elapsed = 0;
+        needs_requeue = 1;
+        TAILQ_REMOVE(&list, curr_process, pointers);
+        // printf("🍅 Pre-empt: current time step = %d\n", curr_time_step);
+        // printf("🍅 -----: pid = %d\n", curr_process->pid);
+      }
+    }
+    curr_time_step += 1;
+  }
+  //Iterate through to add up waiting, response time
+  for (u32 i = 0; i < size; i++)
+  {
+    total_waiting_time += data[i].waiting_time;
+    total_response_time += data[i].response_time;
+  }
   /* End of "Your code here" */
 
   printf("Average waiting time: %.2f\n", (float)total_waiting_time / (float)size);
